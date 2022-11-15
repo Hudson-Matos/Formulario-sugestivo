5a0d76c35d002745c6b760b9c47380d0d8ee3332
Hudson-Matos <matoshudson07@gmail.com>
2022-11-15 19:53:27 -0300

    finalização da ideia principal
    Foi criado uma nova função - verificarCbox(); em que verifica se em cada checkbox existe a classe "requerido", se não existir em nenhuma checkbox, a função vai alterar a cor da fonte para vermelho de cada label correspondente.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const containerAjuste = document.querySelector(".container_ajuste");
+const spanSugestivo = document.querySelector("#span_sugestivo");
 const btnGerarSenha = document.querySelector("#btn_gerar_senha");
 const senhaGerada = document.querySelector("#senha_gerada");
 const cbox1 = document.querySelector("#cbox1");
@@ -6,6 +8,8 @@ const cbox3 = document.querySelector("#cbox3");
 const slider = document.querySelector("#slider");
 const span = document.querySelector("#span");
 const btnCopiar = document.querySelector("#btn_copiar")
+
+spanSugestivo.addEventListener("click", () => {containerAjuste.style.display = "flex";})
 
 span.innerHTML = slider.value;
 span.value = slider.value;
@@ -16,28 +20,41 @@ slider.addEventListener('input', () => {
 
 const cboxList = [cbox1,cbox2,cbox3] 
 cboxList.forEach((item)=>{
-    item.addEventListener('click', () => {item.classList.toggle("requerido")})
+    item.addEventListener('click', () => {item.classList.toggle("requerido"); verificarCbox();})
 })
 
 btnGerarSenha.addEventListener("click", (event) => {
     event.preventDefault();
+    if(verificarCbox()) return;
+
+    document.querySelector("#box_senha").style.display="flex";
     const requeridosList = [];
     for(const item of cboxList) {
         if(item.classList.contains("requerido")) {
             if(item==cbox1) {requeridosList.push(getLetrasMaiusculas)}
             if(item==cbox2) {requeridosList.push(getNumeros)}
-            if(item==cbox3) {requeridosList.push(getSimbolos)}
-        }
-    }
+            if(item==cbox3) {requeridosList.push(getSimbolos)} } }
     senhaGerada.innerHTML = getSenhaAleatoria(requeridosList);
     btnCopiar.innerHTML = "copiar";
-    senhaGerada.parentElement.style.display = "block";
 })
+
 btnCopiar.addEventListener('click', (event) => {
     event.preventDefault();
     navigator.clipboard.writeText(senhaGerada.innerHTML);
     btnCopiar.innerHTML = "copiado";
 })
+
+function verificarCbox() {
+    const classesRequeridas = [];
+    for(const item of cboxList) {
+        if(item.classList.contains("requerido")) {
+            classesRequeridas.push(item); } }
+    if(classesRequeridas.length == 0) {
+        cboxList.forEach((item) => {item.parentElement.style.color = "red";} )
+        return true; }
+    else{
+         cboxList.forEach((item)=> {item.parentElement.style.color = "black"; } ) }
+}
 
 function getLetrasMinusculas() {
     return String.fromCharCode(Math.floor(Math.random()*26+97));
@@ -49,11 +66,10 @@ function getNumeros() {
     return Math.floor(Math.random()*10).toString()
 }
 function getSimbolos() {
-    const simbolos = "!@#$%&*?\|";
+    const simbolos = "!@#$%*?\|";
     return(simbolos[Math.floor(Math.random()*simbolos.length)]);
 }
 function getSenhaAleatoria(requerimentos) {
-    if(!requerimentos.length) return "";
     const quantChar = span.value;
     let senha = "";
     for(let i=0; i<quantChar; i+=requerimentos.length) {
